5b5b00c13a0a5f2c811c48abbd70b01cf663fcd7
EdwinSch <edwinschutjes@gmail.com>
2023-08-28 15:19:11 +0200

    add: create fetch from contenful functionalities

---
+++ src/fetchProjects.jsx
@@ -0,0 +1,35 @@
+import { createClient } from "contentful";
+import { useState, useEffect } from "react";
+
+const client = createClient({
+  space: "p0anhrlepwkt",
+  environment: "master",
+  accessToken: "H0lsYg5zkkR9YidegptE8O_bSLfm4h4DJs1fss5bigY",
+});
+
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: "projects" });
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, id, img };
+      });
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return { loading, projects };
+};
